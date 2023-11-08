8d473f68f49c6c231f2fd76eb899d22d3fb0b020
Etim Iniekung <116564024+Glaciux@users.noreply.github.com>
2023-11-08 17:26:22 +0100

    Delete Day1 - Debug.py

--- Day1 - Debug.py
+++
@@ -1,4 +0,0 @@
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the '+' sign.")
-print("e.g. print('Hello' + 'love')")
-print("New lines can be created with the backslash and n.")
